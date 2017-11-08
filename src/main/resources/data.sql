878c8a1758894208847a9c4755d568efe230d5a6
Tony Jenkins <tony.jenkins@elder-studios.co.uk>
2017-11-08 09:33:53 +0000

    Add retrieval of multiple entries by user.

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,4 @@
 INSERT INTO entries (COMMENT, USER) VALUES ('Great Comment', 'john');
 INSERT INTO entries (COMMENT, USER) VALUES ('Me Too!', 'jane');
 INSERT INTO entries (COMMENT, USER) VALUES ('I agree.', 'alice');
+INSERT INTO entries (COMMENT, USER) VALUES ('Thanks for the add.', 'john');
